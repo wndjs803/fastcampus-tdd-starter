38fb57a2d79173ba2849fc57cd901ca9380e6fb5
김주언 <wndjs803@naver.com>
2024-07-29 21:52:08 +0900

    fail method example

--- src/test/java/com/fastcamp/tddstarter/ch02/AssertEqualsTest.java
+++ src/test/java/com/fastcamp/tddstarter/ch02/AssertEqualsTest.java
@@ -1,8 +1,11 @@
 package com.fastcamp.tddstarter.ch02;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class AssertEqualsTest {
     @Test
@@ -10,5 +13,20 @@ public class AssertEqualsTest {
         int actual = 1;
         int expected = 1;
         assertEquals(expected, actual);
+    }
+
+    private String toUpperCase(String name) throws NullPointerException {
+        if(name == null) throw new NullPointerException();
+        return name.toUpperCase();
+    }
+    @Test
+    void toUpperCaseTest() {
+        try {
+            String name = toUpperCase(null);
+            Assertions.fail();
+            assertEquals("JIHO", name);
+        } catch (NullPointerException e) {
+
+        }
     }
 }
